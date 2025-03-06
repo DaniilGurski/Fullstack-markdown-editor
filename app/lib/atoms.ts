0e24ebb8e2fd6f9a2d63202155e6f6569523bf93
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2025-03-06 20:04:40 +0100

    implement gettings and loading documents.

--- app/lib/atoms.ts
+++ app/lib/atoms.ts
@@ -1,6 +1,17 @@
 import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
+import { DocumentSchema } from "@/app/lib/schemas/document";
+import { TCurrentUserDocument } from "@/app/lib/definitions/states";
+import { z } from "zod";
 
 export const markdownPreviewOpenedAtom = atom(false);
 export const darkModeOnAtom = atomWithStorage("markdownDarkMode", false);
 export const documentPanelOpenedAtom = atom(false);
+
+// An array of documents
+export const userDocumentsAtom = atom<z.infer<typeof DocumentSchema>[]>([]);
+export const currentUserDocumentAtom = atom<TCurrentUserDocument>({
+  documentName: "",
+  content: "",
+  id: "",
+});
---
+++ app/lib/definitions/states.ts
@@ -0,0 +1,5 @@
+export type TCurrentUserDocument = {
+  documentName: string;
+  content: string;
+  id: string;
+};
