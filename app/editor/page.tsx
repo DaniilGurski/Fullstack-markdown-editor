70d764ea50a6aba690aeac870b772800fd7ead49
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2025-03-20 10:19:44 +0100

    dynamic favicon.

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -2,11 +2,25 @@ import Main from "@/app/components/editor/Main";
 import DocumentPanel from "@/app/components/editor/panel/DocumentPanel";
 import MarkdownEditor from "@/app/components/editor/MarkdownEditor";
 import DeleteModal from "@/app/components/editor/DeleteModal";
+// import faviconDark from "@/public/assets/favicon-dark.png";
+// import faviconLight from "@/public/assets/favicon-light.png";
 
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Editor",
+  icons: [
+    {
+      media: "(prefers-color-scheme: light)",
+      url: "/assets/favicon-dark.png",
+      href: "/assets/favicon-dark.png",
+    },
+    {
+      media: "(prefers-color-scheme: dark)",
+      url: "/assets/favicon-light.png",
+      href: "/assets/favicon-light.png",
+    },
+  ],
 };
 
 export default function Page() {
